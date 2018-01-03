838dbec8c1038f091853cc54e5c86c5853086e82
Kyle Ferriter <kylerferriter@gmail.com>
2018-01-03 12:28:56 -0500

    updated worker script to allow for a different toil install path, and added unique id to end of chronos job names to avoid overwriting jobs when multiple are created from same step name

--- _toil_worker.sh
+++ _toil_worker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source venv2.7/bin/activate
-venv2.7/bin/_toil_worker $@
-
+if [ -z $TOIL_INSTALL ]; then
+    export TOIL_INSTALL=/opt/toil/
+fi
+source ${TOIL_INSTALL}/venv2.7/bin/activate
+${TOIL_INSTALL}/venv2.7/bin/_toil_worker $@
