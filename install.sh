fc6a4a186fb6995a7585dcd5c4dca38791a3713a
Kyle Ferriter <kylerferriter@gmail.com>
2018-01-03 15:40:48 -0500

    added aws extra to pip install of toil. This includes necessary packages for cwltoil.

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo pip2.7 install virtualenv
 virtualenv -p python2.7 venv2.7
 source venv2.7/bin/activate
 pip install chronos-python
-pip install -e .[cwl,mesos]
+pip install -e .[cwl,mesos,aws]
 
 sudo ln -s $(pwd)/_toil_worker.sh /usr/bin/_toil_worker
